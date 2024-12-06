507f91012584ac28d4e2a89febb0d396aed9c0ef
therajusah <workwithrajukumar@gmail.com>
2024-12-06 12:00:30 +0530

    icons added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { BsTwitterX } from "react-icons/bs";
 import React from 'react';
-import { MdHomeFilled } from "react-icons/md";
+import { MdHomeFilled, MdOutlineMail, MdOutlinePostAdd } from "react-icons/md";
+import { IoSearch } from "react-icons/io5";
+import { HiOutlineBell } from "react-icons/hi";
+import { CiCircleMore, CiUser } from "react-icons/ci";
 interface XSidebarButton {
   title: string;
   icon: React.ReactNode;
@@ -10,7 +13,32 @@ const SidebarMenuItem: XSidebarButton[] = [
   {
     title: "Home",
     icon: <MdHomeFilled />
-  }
+  },
+  {
+    title: "Search",
+    icon: <IoSearch />
+  },
+  {
+    title: "Notifications",
+    icon: <HiOutlineBell />
+  },
+  {
+    title: "Messages",
+    icon: <MdOutlineMail />
+  },
+  {
+    title: "User",
+    icon: <CiUser />
+  },
+  {
+    title: "More",
+    icon: <CiCircleMore />
+  },
+  {
+    title: "Add Post",
+    icon: <MdOutlinePostAdd />
+
+  },
 ]
 
 
@@ -24,16 +52,15 @@ export default function Home() {
           </div>
           <div className="mt-4">
             <ul>
-            {SidebarMenuItem.map(item => <li className="flex justify-start items-center gap-2 text-white text-4xl pl-2 cursor-pointer hover:bg-gray-600 transition-all rounded-full" key={item.title}><span>{item.icon}</span></li>)}
+            {SidebarMenuItem.map(item => <li className="flex justify-start items-center gap-2 text-white text-4xl lg:pl-2 cursor-pointer hover:bg-slate-400 transition-all rounded-full mt-8" key={item.title}><span>{item.icon}</span></li>)}
          </ul>
          </div>
-
         </div>
 
       
         <div className="col-span-6 border-r-[0.2px] border-l-[0.2px] border-gray-400"></div>
 
-    
+
         <div className="col-span-4"></div>
       </div>
     </div>
